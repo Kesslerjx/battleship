31a8936a4d69b8564eaa9e8245ed1432ba6d6898
Joseph Kessler <kesslerjx@icloud.com>
2022-06-19 20:57:46 -0400

    Add comments to enemy AI code

--- modules/objects/enemy.js
+++ modules/objects/enemy.js
@@ -7,6 +7,9 @@ class Enemy extends Player {
         this.possibleHits  = [];
     }
 
+    //Algorithm to determine which move to play
+    //It will do a random move unless one of the other arays aren't empty
+    //Once a move is tried, it should be removed from the array
     move() {
 
         this.#checkPattern();
@@ -26,11 +29,17 @@ class Enemy extends Player {
         
     }
 
+    //Gets all the adjacent boxes are a hit is registered
+    //Those adjacent boxes must be valid moves
     getAdjacentBoxes(i) {
         this.adjacentBoxes = [i + 10, i-10, i+1, i-1];
         this.adjacentBoxes = this.adjacentBoxes.filter(index => this.isMoveValid(index, i));
     }
 
+    //Notifies the enemy AI that it missed
+    //This only matters if the AI is currently attacking a ship and hasn't sunk it
+    //It will filter out all the possible hits continuing in the direction it was going
+    //That'll force it to go the opposite direction
     missed() {
         if(this.possibleHits.length > 0 ) {
             let nextHit    = this.possibleHits[0];
@@ -46,15 +55,20 @@ class Enemy extends Player {
         }
     }
 
+    //Registers that a ship was sunk and resets the arrays
     shipSunk() {
         this.adjacentBoxes = [];
         this.possibleHits  = [];
     }
 
+    //Gets a random move based on what's in the grid array
     #randomMove() {
         return this.grid[Math.floor(Math.random() * this.grid.length)];
     }
 
+    //Checks to see if there a current pattern in the hits array
+    //This would only matter if the enemy AI is searching for the rest of the ship
+    //Once a pattern is found it will set the possible hits array
     #checkPattern() {
         if(this.hits.length >= 2 && this.possibleHits.length === 0 && this.adjacentBoxes.length > 0) {
             let lastIndex  = this.hits.length - 1;
@@ -68,6 +82,9 @@ class Enemy extends Player {
         }
     }
 
+    //Builds the possible hits array based on the last place hit and the pattern
+    //It will go one direction until the move isn't valid, then it will turn around
+    //This could be recursive, but the last algorithm I had didn't work properly
     #setPossibleHits(pattern, lastHit) {
 
         let next = lastHit + pattern;
@@ -85,6 +102,9 @@ class Enemy extends Player {
         }
     }
 
+    //Determines if the move is valid
+    //It's valid if its included in the grid array
+    //It's also valid if the ship is left to right and the move is on the same row
     #isMoveValid(move, pattern) {
         let previousMove = move - pattern;
         let valid        = this.grid.includes(move);
